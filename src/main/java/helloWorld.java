687a4ee82853424407c84bf58a737f06edb96ee4
SuprunNIX <rostislav.suprun@nixsolutions.com>
2022-08-11 18:18:27 +0300

    hello world

---
+++ src/main/java/helloWorld.java
@@ -0,0 +1,7 @@
+public class helloWorld {
+    public void setHelloWorld() throws InterruptedException {
+        String helloWorld = "Hello World";
+        System.out.println(helloWorld);
+    }
+
+}
